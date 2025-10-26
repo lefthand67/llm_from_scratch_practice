a251d2e1b0ea6fc43ec0c13d6928229a0de63fa1
Vadim Rudakov <lefthand67@yandex.ru>
2025-10-27 03:01:36 +0500

    wip: dataset of files processed now, not one text file

--- 01_tokenizer/bpe_tokenizer.py
+++ 01_tokenizer/bpe_tokenizer.py
@@ -4,10 +4,10 @@ from pathlib import Path
 
 def main():
     """Run the script."""
-    text = Path("../data/text1.txt")
+    dataset_path = "../data/shakespeare_dataset"
 
     my_tokenizer = BPETokenizer()
-    my_tokenizer.train(text)
+    my_tokenizer.train(dataset_path)
 
 
 class BPETokenizer:
@@ -19,107 +19,130 @@ class BPETokenizer:
         self.vocab = dict()
         # decoding token -> id
         self.reverse_vocab = dict()
-        # (token1, token2): merged_token
-        self.merges = dict()
         self.next_id = 0
 
-    def _initialize_vocab(self, text):
-        """Start with individual characters."""
-        with open(text, "r") as f:
-            chars = sorted(set(f.read()))
+    def _initialize_vocab(self, dataset_path):
+        """Initialize vocab with individual characters from entire dataset."""
+        chars = set()
+        for file in dataset_path.iterdir():
+            with open(file, "r") as f:
+                chars.update(set(f.read()))
 
+        chars = sorted(chars)
         self.vocab = {char: i for i, char in enumerate(chars)}
         self.reverse_vocab = {i: char for char, i in self.vocab.items()}
         self.next_id = len(self.vocab)
 
-    def _tokenize_text(self, text):
-        """Prepare text database for token representation."""
-        with open(text, "r") as f:
-            lines = f.readlines()
-
+    def _tokenize_dataset(self, dataset_path):
+        """Tokenize entire database."""
         # final list of tokenized lines
-        tokenized_text = list()
+        tokenized_dataset = list()
 
-        # convert each vocab character to its token representation
-        for line in lines:
-            # each line is the list of tokens
-            tokenized_line = [self.vocab[c] for c in line]
+        for file in dataset_path.iterdir():
+            with open(file, "r") as f:
+                text = f.read()
+
+            # convert each vocab character to its token representation
+            tokenized_text = [self.vocab[c] for c in text]
             # populate the final list with the new converted line
-            tokenized_text.append(tokenized_line)
+            tokenized_dataset.append(tokenized_text)
 
-        return tokenized_text
+        return tokenized_dataset
 
-    def _count_pairs(self, tokenized_text):
-        """Count token pairs across entire database."""
+    def _count_pairs(self, tokenized_dataset):
+        """Count token pairs across entire dataset."""
         # initialize a dictionary for counting occurences
         count_pairs_dict = defaultdict(int)
         # max_count value
         max_count = 0
 
-        for line in tokenized_text:
-            for i in range(len(line) - 1):
-                pair = (line[i], line[i + 1])
+        for text in tokenized_dataset:
+            for i in range(len(text) - 1):
+                pair = (text[i], text[i + 1])
                 # update count
                 count_pairs_dict[pair] += 1
                 # update max_count value
-                if count_pairs_dict[pair] > max_count:
-                    max_count = count_pairs_dict[pair]
+                pair_count = count_pairs_dict[pair]
+                if pair_count > max_count:
+                    max_count = pair_count
 
         return max_count, count_pairs_dict
 
-    def _merge_pair(self, tokenized_text, merge_pair):
-        # update tokenized text
-        for i in range(len(tokenized_text)):
-            line = tokenized_text[i]
-            # print("1", tokenized_text[i])
-
-            # updated list of tokens
-            updated_line = list()
-
-            # len line - 2 for staying within range
-            len_line = len(line) - 2
-
-            # keep empty lines as is
-            if len_line == 0:
-                updated_line.append(line)
-                continue
-
-            # find paired token's position and populate new line
-            idx = 0
-            while idx <= len_line:
-                if (line[idx], line[idx + 1]) == merge_pair:
-                    updated_line.append(self.merges[merge_pair])
-                    idx += 1
-                else:
-                    updated_line.append(line[idx])
-                idx += 1
-            tokenized_text[i] = updated_line
-            # print("2", tokenized_text[i])
-
-        print(tokenized_text[6])
-
-        return tokenized_text
-
-    def train(self, text):
-        """Convert text to initial tokens."""
-        self._initialize_vocab(text)
-        tokenized_text = self._tokenize_text(text)
-
-        # get pairs information
-        max_count, count_pairs_dict = self._count_pairs(tokenized_text)
-
+    def _get_candidate_pair(self, max_count, count_pairs_dict):
+        """Find a new pair for merge."""
+        # find most frequent pairs
         popular_pairs = list()
         for pair, count in count_pairs_dict.items():
             # print(f"{self.reverse_vocab[pair[0]]}{self.reverse_vocab[pair[1]]}")
+            # append popular_pairs list
             if count == max_count:
                 popular_pairs.append(pair)
 
-        # for pair in popular_pairs:
-        #    print(self.reverse_vocab[pair[0]], self.reverse_vocab[pair[1]])
+        candidate_pair = min(popular_pairs)
 
-        merge_pair = min(popular_pairs)
-        self.merges[merge_pair] = self.next_id
+        print(f"candidate pair: {candidate_pair}")
+        print(f"max count: {max_count}")
+
+        return candidate_pair
+
+    def _update_vocab(self, candidate_pair):
+        # update vocab with new id-token pair
+        self.vocab[candidate_pair] = self.next_id
+        self.reverse_vocab[self.next_id] = candidate_pair
+
         self.next_id += 1
+
+    def _merge_pair(self, tokenized_dataset, candidate_pair):
+        """Update tokenized_dataset with the merged pair."""
+        # update dataset
+        check_max_count = 0
+        for i in range(len(tokenized_dataset)):
+            # original tokenized text
+            text = tokenized_dataset[i]
+            # updated tokenized text
+            updated_text = list()
+
+            # find paired tokens' position and populate new text
+            idx = 0
+            # -2 because idx starts from 0 + we need idx+1
+            while idx <= len(text) - 2:
+                if (text[idx], text[idx + 1]) == candidate_pair:
+                    updated_text.append(self.vocab[candidate_pair])
+                    idx += 1
+                    check_max_count += 1
+                else:
+                    updated_text.append(text[idx])
+                idx += 1
+
+            # update text in dataset
+            tokenized_dataset[i] = updated_text
+
+        # check
+        print(f"check_max_count: {check_max_count}")
+
+        print(self.vocab)
+        print(self.reverse_vocab)
+
+        return tokenized_dataset
+
+    def train(self, dataset: str):
+        """Convert text to initial tokens."""
+        dataset_path = Path(dataset)
+
+        self._initialize_vocab(dataset_path)
+        tokenized_dataset = self._tokenize_dataset(dataset_path)
+
+        # get pairs information
+        max_count, count_pairs_dict = self._count_pairs(tokenized_dataset)
+
+        # get new candidate pair
+        candidate_pair = self._get_candidate_pair(max_count, count_pairs_dict)
+
+        # update vocab
+        self._update_vocab(candidate_pair)
+
+        # merge pair into a new token
+        self._merge_pair(tokenized_dataset, candidate_pair)
 
 
 if __name__ == "__main__":
