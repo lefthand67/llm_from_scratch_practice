463aa8fb853211bf662feeefe8903968263fcc29
Vadim Rudakov <lefthand67@yandex.ru>
2025-10-29 01:53:58 +0500

    feat: string-based train implemented
    my previous approach of training on integer-based text was incorrect from production point of view;
    I added self.merges list which stores rules for merging, string-based, so now I am ready for writing the encode method for tokenizing new texts;
    string-based training is needed for inference, integer-based training overcomplicates inference making any optimization meaningless

--- 01_tokenizer/bpe_tokenizer.py
+++ 01_tokenizer/bpe_tokenizer.py
@@ -1,7 +1,3 @@
-from collections import defaultdict
-from pathlib import Path
-
-
 def main():
     """Run the script."""
     dataset_path = "../data/shakespeare_dataset"
@@ -10,20 +6,27 @@ def main():
     my_tokenizer.train(dataset_path, vocab_size=200, frequency_threshold=20)
 
     print("Final vocabulary size:", len(my_tokenizer.vocab))
+
     print("Vocabulary learned:")
     print(my_tokenizer.vocab)
+
+    print("Merges:")
+    print(my_tokenizer.merges)
 
 
 class BPETokenizer:
     """BPETokenizer class definition."""
 
-    def __init__(self):
+    def __init__(self, special_tokens: tuple = ("</unk>", "<s>", "</s>")):
         """Initialize the class entity."""
-        # encoding id -> token
-        self.vocab = dict()
-        # decoding token -> id
-        self.reverse_vocab = dict()
-        self.next_token = 0
+        self.special_tokens = special_tokens
+        # token -> token_id
+        self.vocab: dict[str, int] = dict()
+        # decoding token_id -> token
+        self.reverse_vocab: dict[int, str] = dict()
+        self.next_token: int = 0
+        # ordered list for storing merging rules ('a', 'c')
+        self.merges: list[tuple] = list()
 
     def _initialize_vocab(self, dataset_path):
         """Initialize vocab with individual characters from entire dataset."""
@@ -34,12 +37,11 @@ class BPETokenizer:
 
         chars = sorted(chars)
 
-        # unkown, start, end special tokens
-        self.vocab = {"</unk>": 0, "<s>": 1, "</s>": 2}
+        # special tokens first
+        for token in self.special_tokens:
+            self.vocab[token] = self.next_token
+            self.next_token += 1
         self.reverse_vocab = {i: char for char, i in self.vocab.items()}
-
-        # fix next_token
-        self.next_token = len(self.vocab)
 
         # populate vocabs with chars from dataset
         for char in chars:
@@ -47,30 +49,28 @@ class BPETokenizer:
             self.reverse_vocab[self.next_token] = char
             self.next_token += 1
 
-    def _tokenize_dataset(self, dataset_path):
-        """Tokenize entire database."""
-        # final list of tokenized lines
-        tokenized_dataset = list()
+    def _prepare_dataset(self, dataset_path):
+        """Make list of texts broken into single chars."""
+        # final list of tokenized texts
+        prepaired_dataset = list()
 
         for file in dataset_path.iterdir():
             with open(file, "r") as f:
-                text = f.read()
+                # populate the final list with the new converted line
+                prepaired_dataset.append([char for char in f.read()])
 
-            # convert each vocab character to its token representation
-            tokenized_text = [self.vocab[c] for c in text]
-            # populate the final list with the new converted line
-            tokenized_dataset.append(tokenized_text)
+        return prepaired_dataset
 
-        return tokenized_dataset
-
-    def _count_pairs(self, tokenized_dataset):
+    def _count_pairs(self, prepaired_dataset: list):
         """Count token pairs across entire dataset."""
+        from collections import defaultdict
+
         # initialize a dictionary for counting occurences
-        count_pairs_dict = defaultdict(int)
+        count_pairs_dict: dict[tuple, int] = defaultdict(int)
         # max_count value
         max_count = 0
 
-        for text in tokenized_dataset:
+        for text in prepaired_dataset:
             for i in range(len(text) - 1):
                 pair = (text[i], text[i + 1])
                 # update count
@@ -82,7 +82,7 @@ class BPETokenizer:
 
         return max_count, count_pairs_dict
 
-    def _get_merge_candidate(self, max_count, count_pairs_dict):
+    def _get_merge_candidate(self, max_count: int, count_pairs_dict: dict):
         """Find a new pair for merge."""
         # find most frequent pairs
         popular_pairs = list()
@@ -95,43 +95,55 @@ class BPETokenizer:
 
         return merge_candidate
 
-    def _update_vocab(self, merge_candidate):
+    def _append_merges(self, merge_candidate: tuple):
+        """Append new rule to self.merges list."""
+        self.merges.append(merge_candidate)
+
+    def _update_vocab(self, merge_candidate: tuple):
         """
         Update both vocab and reverse_vocab.
 
-        Return: new_pair_id: str
+        Parameters
+        ----------
+        merge_candidate : tuple
+          the most popular pair; lexicographically first
+
+        Return
+        ------
+        merged_string : str
+
         """
         # convert merge_candidate into char pair
-        new_pair_id = f"{self.reverse_vocab[merge_candidate[0]]}{self.reverse_vocab[merge_candidate[1]]}"
+        merged_string = f"{merge_candidate[0]}{merge_candidate[1]}"
 
         # update vocab with new id-token pair
-        self.vocab[new_pair_id] = self.next_token
-        self.reverse_vocab[self.next_token] = new_pair_id
+        self.vocab[merged_string] = self.next_token
+        self.reverse_vocab[self.next_token] = merged_string
 
         self.next_token += 1
 
-        return new_pair_id
+        return merged_string
 
-    def _update_tokenized_dataset(
-        self, tokenized_dataset: list, merge_candidate: tuple, pair_id: str
+    def _update_prepaired_dataset(
+        self, prepaired_dataset: list, merge_candidate: tuple, merged_string: str
     ):
         """
-        Update tokenized_dataset with the merged pair.
+        Update prepaired_dataset with the merged pair.
 
         Parameters
         ----------
-        tokenized_dataset : list
+        prepaired_dataset : list
             A list of tokenized texts.
         merge_candidate : tuple
             A pair of tokens to be merged.
-        pair_id : str
+        merged_string : str
             A pair of characters from self.vocab.
 
         """
         # update dataset
-        for i in range(len(tokenized_dataset)):
+        for i in range(len(prepaired_dataset)):
             # original tokenized text
-            text = tokenized_dataset[i]
+            text = prepaired_dataset[i]
             # updated tokenized text
             updated_text = list()
 
@@ -140,16 +152,16 @@ class BPETokenizer:
             # -2 because idx starts from 0 + we need idx+1
             while idx <= len(text) - 2:
                 if (text[idx], text[idx + 1]) == merge_candidate:
-                    updated_text.append(self.vocab[pair_id])
+                    updated_text.append(merged_string)
                     idx += 1
                 else:
                     updated_text.append(text[idx])
                 idx += 1
 
             # update text in dataset
-            tokenized_dataset[i] = updated_text
+            prepaired_dataset[i] = updated_text
 
-        return tokenized_dataset
+        return prepaired_dataset
 
     def train(self, dataset: str, vocab_size: int, frequency_threshold: int = 2):
         """
@@ -169,16 +181,18 @@ class BPETokenizer:
             safety measure to avoid rare frequencies to be added to vocab, not including.
 
         """
+        from pathlib import Path
+
         dataset_path = Path(dataset)
 
         self._initialize_vocab(dataset_path)
 
-        tokenized_dataset = self._tokenize_dataset(dataset_path)
+        prepaired_dataset = self._prepare_dataset(dataset_path)
 
         iter_number = vocab_size - len(self.vocab)
         for _ in range(iter_number):
             # get pairs information
-            max_count, count_pairs_dict = self._count_pairs(tokenized_dataset)
+            max_count, count_pairs_dict = self._count_pairs(prepaired_dataset)
 
             if max_count < frequency_threshold:
                 print("No more pairs to merge available. Exiting...")
@@ -187,12 +201,15 @@ class BPETokenizer:
             # get new candidate pair
             merge_candidate = self._get_merge_candidate(max_count, count_pairs_dict)
 
+            # append the sequence to self.merges
+            self._append_merges(merge_candidate)
+
             # update vocab
-            new_pair_id = self._update_vocab(merge_candidate)
+            merged_string = self._update_vocab(merge_candidate)
 
             # merge pair into a new token
-            tokenized_dataset = self._update_tokenized_dataset(
-                tokenized_dataset, merge_candidate, new_pair_id
+            prepaired_dataset = self._update_prepaired_dataset(
+                prepaired_dataset, merge_candidate, merged_string
             )
 
 
