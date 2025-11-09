da0f26dab62e1eb350e80cdb51fccaa2b72a84de
Vadim Rudakov <lefthand67@yandex.ru>
2025-11-09 16:42:07 +0500

    WIP: finalizing the workbook after session ended

--- F_nn_from_scratch/neuron.py
+++ F_nn_from_scratch/neuron.py
@@ -1,30 +1,6 @@
 import numpy as np
 
 
-def main():
-    """Run code."""
-    X = np.array(
-        [
-            [-1, 2, 3, 13, 17],
-            [4, -5, 6, 14, 18],
-            [7, 8, -9, 15, 19],
-            [-10, 11, 12, 16, -20],
-        ],
-        dtype=np.float64,
-    )
-    y = np.array([100, 200, 300, 400], dtype=np.float64)
-
-    my_neuron = Neuron((X.shape))
-    print("my_neuron.input_size:", my_neuron.input_size)
-    print("my_neuron.w:", my_neuron.w)
-    print("linear transformation:", my_neuron._get_linear_transformation(X))
-    print("my_neuron.forward(X):", my_neuron.forward(X))
-    print(
-        "my_neuron.gradient_of_J(y_true=y, x=X):",
-        my_neuron.gradient_of_J(y_true=y, x=X),
-    )
-
-
 class Neuron:
     """Implement single neuron class."""
 
@@ -84,7 +60,3 @@ class Neuron:
         )
 
         return np.dot((error * leaky_relu_derivative), x)
-
-
-if __name__ == "__main__":
-    main()
--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 *"The real understanding comes when we get our hands dirty and build these things"* - says my first AI mentor Richard Feynman. 
 
+The goal of this course is a preparation for AI-backend optimization (e.g., CUDA, tensor cores, memory hierarchy tuning).
+
 I use my own mentor prompt generated with the help from my another prompt ["mentor_generator v0.24.3"](https://github.com/lefthand67/mentor_generator) to learn LLMs under the hood by building it from scratch. The idea stems from the [Stanford CS336 course](https://www.youtube.com/playlist?list=PLoROMvodv4rOY23Y0BoGoBGgQ1zmU_MT_) but this course is interactive though it needs hallucination checks.
 
 I run my mentor in free DeepSeek chat in no reasoning mode (the system prompt is already too heavy). I am excited to see where this experiment will lead me to.
