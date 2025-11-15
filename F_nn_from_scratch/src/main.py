5fafbc9f28e073cbcfdab8440807645cc0e3b6c4
Vadim Rudakov <lefthand67@yandex.ru>
2025-11-15 23:12:30 +0500

    WIP: working on F phase

--- F_nn_from_scratch/src/main.py
+++ F_nn_from_scratch/src/main.py
@@ -17,18 +17,13 @@ def main():
     y = np.array([100, 200, 300, 400], dtype=np.float64)
 
     my_neuron = Neuron()
-    my_neuron.gradient(X, y)
+    dJ_dw, dJ_db = my_neuron.gradient(y_true=y, X=X)
 
-    # print("my_neuron.input_size:", my_neuron.input_size)
-    print("my_neuron.w:", my_neuron.w)
-    print("my_neuron.b:", my_neuron.b)
-    # print("linear transformation:", my_neuron._get_linear_transformation(X))
+    print("weights:", my_neuron.w)
     print("linear transformation:", my_neuron.z)
-    print("Activation transformation:", my_neuron.a)
-    print(
-        "my_neuron.gradient(y_true=y, X=X):",
-        my_neuron.gradient(X=X, y_true=y),
-    )
+    print("activation:", my_neuron.a)
+    print("dJ_dw:", dJ_dw)
+    print("dJ_db:", dJ_db)  # print("my_neuron.input_size:", my_neuron.input_size)
 
 
 if __name__ == "__main__":
--- F_nn_from_scratch/src/neuron.py
+++ F_nn_from_scratch/src/neuron.py
@@ -7,7 +7,7 @@ class Neuron:
     def __init__(self: "Neuron") -> None:
         """Initialize neuron entity."""
         self.w: np.ndarray = None
-        self.b: np.float64 = np.float64(0.0)
+        self.b: np.float32 = np.float32(0.0)
         # linear transformation
         self.z: np.ndarray = None
         # activation transformation
@@ -17,64 +17,85 @@ class Neuron:
         # learning rate for weights update
         self.lr: np.float32 = np.float32(0.01)
 
+    def activation(
+        self,
+        alpha: np.float32 = None,
+        activation_algorithm: str = "leaky_relu",
+    ):
+        """Break linearity."""
+        if alpha is not None:
+            self.alpha = alpha
+        if activation_algorithm == "leaky_relu":
+            self.a = np.maximum(self.alpha * self.z, self.z)
+        else:
+            raise ValueError(
+                f"Activation function {activation_algorithm} not implemented (yet)"
+            )
+
     def forward(
         self,
         X: np.ndarray,
         alpha: np.float32 = None,
-        activation_f: str = "leaky_relu",
+        activation_algorithm: str = "leaky_relu",
     ) -> np.ndarray:
         """Calculate forward pass with activation function."""
         # Xavier/Glorot weights initialization
         if self.w is None:
             # size of one example
-            input_size = X.shape[-1]
-            self.w = (np.random.default_rng().random(input_size) - 0.5) * np.sqrt(
-                2.0 / input_size
+            self.input_size = X.shape[-1]
+            self.w = (np.random.default_rng().random(self.input_size) - 0.5) * np.sqrt(
+                2.0 / self.input_size
             )
 
         # compute linear transformation
         self.z = np.dot(X, self.w) + self.b
 
         # compute activation transformation
-        if alpha is not None:
-            self.alpha = alpha
-        if activation_f == "leaky_relu":
-            self.a = np.maximum(self.alpha * self.z, self.z)
-        else:
-            raise ValueError(
-                f"Activation function {activation_f} not implemented (yet)"
-            )
+        self.activation(alpha, activation_algorithm)
 
         return self.a
+
+    def gradient_of_a(self, activation_algorithm: str = "leaky_relu"):
+        """Compute a'."""
+        if activation_algorithm == "leaky_relu":
+            da_dz = np.where(self.z < 0, self.alpha, 1)
+        else:
+            raise ValueError(
+                f"Activation function {activation_algorithm} not implemented (yet)"
+            )
+
+        return da_dz
 
     def gradient(
         self,
         X: np.ndarray,
         y_true: np.ndarray,
         alpha: np.float32 = None,
-        activation_f: str = "leaky_relu",
+        activation_algorithm: str = "leaky_relu",
     ) -> np.ndarray:
         """Gradients for squared error loss: J = (y_pred - y_true)^2 / 2."""
-        if alpha is not None:
-            self.alpha = alpha
+        # X and y_pred shape check
+        batch_size = X.shape[0]
+        y_true_number = y_true.shape[0]
+        if batch_size != y_true.shape[0]:
+            raise ValueError(
+                f"Batch size of X={batch_size} must be of y_pred={y_true_number} size."
+            )
 
         # make forward pass
         self.forward(
             X,
-            activation_f=activation_f,
+            alpha=alpha,
+            activation_algorithm=activation_algorithm,
         )
 
-        # compute J'
-        dJ_da = self.a - y_true
+        # compute J', where J is MSE
+        dJ_da = (self.a - y_true) / batch_size
 
         # compute a'
-        if activation_f == "leaky_relu":
-            da_dz = np.where(self.z < 0, self.alpha, 1)
-        else:
-            raise ValueError(
-                f"Activation function {activation_f} not implemented (yet)"
-            )
+        da_dz = self.gradient_of_a(activation_algorithm=activation_algorithm)
 
+        # compute complete gradients
         dJ_dw = np.dot((dJ_da * da_dz), X)
         dJ_db = np.sum(dJ_da * da_dz)
 
@@ -94,9 +115,9 @@ class TestNeuron:
         X: np.ndarray = np.array([[1.0, 2.0]]),
         y_true: np.ndarray = np.array([1.5]),
         w: np.ndarray = np.array([0.3, 0.4]),
-        b: np.float64 = np.float64(0.1),
+        b: np.float32 = np.float32(0.1),
         lr: np.float32 = np.float32(0.01),
-        activation_f: str = "leaky_relu",
+        activation: str = "leaky_relu",
         cost_f: str = "mse",
     ):
         """
@@ -120,23 +141,25 @@ class TestNeuron:
 
         # activation function
         alpha = 0.01
-        if activation_f == "leaky_relu":
+        if activation == "leaky_relu":
             a = np.maximum(z * alpha, z)
         result["a"] = a
 
-        # cost function
+        # backpropagation
+
+        # cost function and its derivative
+        batch_size = X.shape[0]
         if cost_f == "mse":
+            # error
             u = a - y_true
-            J = u**2 / 2
+            # cost function
+            J = (1 / 2) * (1 / batch_size) * u**2
+            # J'
+            dJ_da = (1 / batch_size) * u
         else:
             raise ValueError(f'Cost function "{cost_f}" is not implemented yet.')
 
         result["J"] = J
-
-        # backpropagation
-
-        # J'
-        dJ_da = u
         result["dJ_da"] = dJ_da
 
         # a'
